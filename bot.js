70500668e488b10238615ae3b2c0462c314fda70
houssam199 <40376744+houssam199@users.noreply.github.com>
2018-09-20 00:12:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`look at me .`,"http://twitch.tv/S-F")
+client.user.setGame(`LOOK AT ME  .`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
